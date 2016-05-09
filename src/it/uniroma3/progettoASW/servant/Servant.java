00e82f2875e8d074b6de1891b74771559e8b70be
carlo <carlo.laviola@gmail.com>
2016-05-09 13:35:37 +0200

    Modifiche al servant

--- src/it/uniroma3/progettoASW/servant/Servant.java
+++ src/it/uniroma3/progettoASW/servant/Servant.java
@@ -1,9 +1,12 @@
 package it.uniroma3.progettoASW.servant;
 
 import java.util.logging.Logger;
+
+import org.hibernate.TransactionException;
+
 import it.uniroma3.progettoASW.service.*;
 import it.uniroma3.progettoASW.enums.Genre;
-import it.uniroma3.progettoASW.server.persistence.facade.MovieFacade;
+import it.uniroma3.progettoASW.server.persistence.facade.*;
 
 public class Servant implements Service {
 
@@ -33,15 +36,20 @@ public class Servant implements Service {
 		String director=args[2];
 		int length=Integer.parseInt(args[3]);
 		Genre genre=Genre.valueOf(args[4]);
-		facade.addMovie(title,length,director,year,genre);
-		return null;
+		
+		
+		try {
+			facade.addMovie(title,length,director,year,genre);
+			this.logger.info("Transaction completed");
+			return "Completed";
+			}
+		
+	catch (TransactionException e){
+		this.logger.info("TransactionException while inserting in DB" + e.getMessage());
+		return "Failed";
 	}
-
-	//riceve la risposta dell'inserimento dalla facade e la comunica al client
-	//	public String setResponse(){
-	//		
-	//		
-	//	}
+		
+	}
 
 
 }
